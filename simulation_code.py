9b5f9b937a1f52916cc82c3981ac8b89f198fad7
Shrish Choudhary <emailshrish@gmail.com>
2024-06-28 21:12:42 -0400

    Create simulation_code.py

---
+++ simulation_code.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2022, UFACTORY, Inc.
+# All rights reserved.
+#
+# Author: Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
+
+"""
+# Notice
+#   1. Changes to this file on Studio will not be preserved
+#   2. The next conversion will overwrite the file with the same name
+# 
+# xArm-Python-SDK: https://github.com/xArm-Developer/xArm-Python-SDK
+#   1. git clone git@github.com:xArm-Developer/xArm-Python-SDK.git
+#   2. cd xArm-Python-SDK
+#   3. python setup.py install
+"""
+import sys
+import math
+import time
+import queue
+import datetime
+import random
+import traceback
+import threading
+from xarm import version
+from xarm.wrapper import XArmAPI
+
+
+class RobotMain(object):
+    """Robot Main Class"""
+    def __init__(self, robot, **kwargs):
+        self.alive = True
+        self._arm = robot
+        self._ignore_exit_state = False
+        self._tcp_speed = 100
+        self._tcp_acc = 2000
+        self._angle_speed = 20
+        self._angle_acc = 500
+        self._vars = {}
+        self._funcs = {}
+        self._robot_init()
+
+    # Robot init
+    def _robot_init(self):
+        self._arm.clean_warn()
+        self._arm.clean_error()
+        self._arm.motion_enable(True)
+        self._arm.set_mode(0)
+        self._arm.set_state(0)
+        time.sleep(1)
+        self._arm.register_error_warn_changed_callback(self._error_warn_changed_callback)
+        self._arm.register_state_changed_callback(self._state_changed_callback)
+        if hasattr(self._arm, 'register_count_changed_callback'):
+            self._arm.register_count_changed_callback(self._count_changed_callback)
+
+    # Register error/warn changed callback
+    def _error_warn_changed_callback(self, data):
+        if data and data['error_code'] != 0:
+            self.alive = False
+            self.pprint('err={}, quit'.format(data['error_code']))
+            self._arm.release_error_warn_changed_callback(self._error_warn_changed_callback)
+
+    # Register state changed callback
+    def _state_changed_callback(self, data):
+        if not self._ignore_exit_state and data and data['state'] == 4:
+            self.alive = False
+            self.pprint('state=4, quit')
+            self._arm.release_state_changed_callback(self._state_changed_callback)
+
+    # Register count changed callback
+    def _count_changed_callback(self, data):
+        if self.is_alive:
+            self.pprint('counter val: {}'.format(data['count']))
+
+    def _check_code(self, code, label):
+        if not self.is_alive or code != 0:
+            self.alive = False
+            ret1 = self._arm.get_state()
+            ret2 = self._arm.get_err_warn_code()
+            self.pprint('{}, code={}, connected={}, state={}, error={}, ret1={}. ret2={}'.format(label, code, self._arm.connected, self._arm.state, self._arm.error_code, ret1, ret2))
+        return self.is_alive
+
+    @staticmethod
+    def pprint(*args, **kwargs):
+        try:
+            stack_tuple = traceback.extract_stack(limit=2)[0]
+            print('[{}][{}] {}'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), stack_tuple[1], ' '.join(map(str, args))))
+        except:
+            print(*args, **kwargs)
+
+    @property
+    def arm(self):
+        return self._arm
+
+    @property
+    def VARS(self):
+        return self._vars
+
+    @property
+    def FUNCS(self):
+        return self._funcs
+
+    @property
+    def is_alive(self):
+        if self.alive and self._arm.connected and self._arm.error_code == 0:
+            if self._ignore_exit_state:
+                return True
+            if self._arm.state == 5:
+                cnt = 0
+                while self._arm.state == 5 and cnt < 5:
+                    cnt += 1
+                    time.sleep(0.1)
+            return self._arm.state < 4
+        else:
+            return False
+
+    # Robot Main Run
+    def run(self):
+        try:
+            # Install xArm Gripper
+            code = self._arm.set_counter_reset()
+            if not self._check_code(code, 'set_counter_reset'):
+                return
+            self._tcp_speed = 200
+            self._tcp_acc = 5000
+            code = self._arm.set_servo_angle(angle=[101.0, -11.1, -56.4, 0.0, 67.5, 24.5],is_radian=False, speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+            if not self._check_code(code, 'set_servo_angle'):
+                return
+            print("pos",self._arm.get_position(False)[1])
+
+            code = self._arm.set_gripper_position(800, wait=True, speed=5000, auto_enable=True)
+            if not self._check_code(code, 'set_gripper_position'):
+                return
+            
+            if not self.is_alive:
+                return
+            t1 = time.monotonic()
+            code = self._arm.set_counter_increase()
+            if not self._check_code(code, 'set_counter_increase'):
+                return
+            # Set TCP payload as the xArm Gripper
+            code = self._arm.set_tcp_load(0.82, [0, 0, 48])
+            if not self._check_code(code, 'set_tcp_load'):
+                return
+
+            print("*************************************************************************")
+            code,temp_pos=self._arm.get_forward_kinematics(
+                [101.0, -11.1, -56.4, 0.0, 67.5, 24.5],False,False)
+            code = self._arm.set_position(*temp_pos,is_radian=False, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=0.0, wait=True)
+            print(temp_pos)
+            if not self._check_code(code, 'set_position'):
+                return
+            print("**********************************************")
+            # code,pos_to_move_to=self._arm.get_inverse_kinematics(
+            #     temp_pos,False,False)
+            coor=[280.10417938232422, 412.1002197265625, 400.00927734375, -180.00000500895632, 0, 0]
+            # print(pos_to_move_to)
+            code = self._arm.set_position_aa(coor,is_radian=False, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=0.0, wait=True)
+            if not self._check_code(code, 'set_position'):
+                return
+            code=self._arm.set_pause_time(2,wait=True)
+            coor=[280.10417938232422, 412.1002197265625, 100, -180.00000500895632, 0, 0]
+            code = self._arm.set_position_aa(coor,is_radian=False, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=0.0, wait=True)
+            if not self._check_code(code, 'set_position_aa'):
+                return
+
+            coor=[280.10417938232422, 412.1002197265625, 400, -180.00000500895632, 0, 0]
+            code = self._arm.set_position_aa(coor,is_radian=False, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=0.0, wait=True)
+            if not self._check_code(code, 'set_position_aa'):
+                return
+            coor=[100.10417938232422, 100.1002197265625, 600, -180.00000500895632, 0, 0]
+            code = self._arm.set_position_aa(coor,is_radian=False, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=0.0, wait=True)
+            if not self._check_code(code, 'set_position_aa'):
+                return
+            interval = time.monotonic() - t1
+            if interval < 0.01:
+                time.sleep(0.01 - interval)
+        except Exception as e:
+            self.pprint('MainException: {}'.format(e))
+        finally:
+            self.alive = False
+            self._arm.release_error_warn_changed_callback(self._error_warn_changed_callback)
+            self._arm.release_state_changed_callback(self._state_changed_callback)
+            if hasattr(self._arm, 'release_count_changed_callback'):
+                self._arm.release_count_changed_callback(self._count_changed_callback)
+
+
+if __name__ == '__main__':
+    RobotMain.pprint('xArm-Python-SDK Version:{}'.format(version.__version__))
+    arm = XArmAPI('172.17.0.2', baud_checkset=False)
+    robot_main = RobotMain(arm)
+    robot_main.run()
